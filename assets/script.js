659dcafe4687e6f5ab44ee7382d69ff3e4511cb8
Hassan Abdirahman <hassanmahdi58@gmail.com>
2022-02-22 19:04:48 +0000

    main

--- assets/script.js
+++ assets/script.js
@@ -5,20 +5,23 @@ var passwordText = document.querySelector("#securepassword");
 
 generateBtn.addEventListener("click", enterPassword);
 
-function generatePassword(){ 
-var length = prompt (" Please enter 8 to 128 number for the password");
+let lowercaseOptions = "abcdefghijklmnopqrstuvwxyz"
+let uppercaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let numberOptions = "0123456789"
+let symbolOptions = "!%&,*+-./<>?~"
 
-  
+function generatePassword(){ 
+var length = prompt (" Please enter 8 to 128 number for the password"); 
 // the length for the password 
 while(length < 8 || length > 128){
   alert("Please Try again the passwod");
   var length= prompt("This  should be  8 and 128 numbers");
 }
 // user can decide which these want to have in the password variable
-let lowercaseOptions = "abcdefghijklmnopqrstuvwxyz"
-let uppercaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let numberOptions = "0123456789"
-let symbolOptions = "!%&,*+-./<>?~"
+var lowercase = confirm ("Click Ok to have lowercase");
+var uppercase = confirm ("Click Ok to have uppercase");
+var numbers = confirm ("Click Ok to have numbers");
+var symbols = confirm ("Click Ok to have symbols");
 
 console.log({})
 
@@ -30,14 +33,7 @@ console.log(useroptions)
 
 useroptions[Math.floor(Math.random()*useroptions.length)];
 
-lowercaseOptions[Math.floor(Math.random()*lowercaseOptions.length)];
 
-let password = useroptions[Math.floor(Math.random()*useroptions.length)];
-
-password = password + useroptions[Math.floor(Math.random()*useroptions.length)];
-
-
-  
 
 
 // create the password
